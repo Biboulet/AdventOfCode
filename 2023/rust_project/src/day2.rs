92da01514850c0e24935486a3b4e6f45d25e96a2
Biboulet <63183586+Biboulet@users.noreply.github.com>
2023-12-02 12:59:18 +0100

    day 2

--- 2023/rust_project/src/day2.rs
+++ 2023/rust_project/src/day2.rs
@@ -1,10 +1,10 @@
+use std::cmp::max;
+use std::cmp::min;
 use std::usize;
-
 use aoc_runner_derive::aoc;
 use aoc_runner_derive::aoc_generator;
 use itertools::Itertools;
 
-type Grab<'a> = (usize, &'a str);
 
 fn parse_input(input: &str) -> Vec<Vec<Vec<(usize, &str)>>> {
     return input.split("\n").map(|line| parse_line(line)).collect_vec();
@@ -27,7 +27,7 @@ fn parse_packet(packet: &str) -> Vec<(usize, &str)> {
         .collect_vec();
 }
 
-fn parse_grab(num_and_color: &str) -> Grab {
+fn parse_grab(num_and_color: &str) -> (usize, &str) {
     let args: Vec<&str> = num_and_color.trim().split(' ').collect();
     return (args[0].parse::<usize>().expect("failed parsing"), args[1]);
 }
@@ -46,6 +46,41 @@ fn solve_part1(input: &str) -> usize {
         })
         .sum();
 }
+#[aoc(day2, part2)]
+fn solve_part2(input: &str) -> usize {
+    let parsed_input: Vec<Vec<Vec<(usize, &str)>>> = parse_input(input);
+    return parsed_input.iter().map(|game| get_game_power(game)).sum();
+}
+
+fn get_game_power(game: &Vec<Vec<(usize, &str)>>) -> usize {
+    let (minRed, minGreen, minBlue) = get_min_num_of_cube(game);
+    return minRed * minGreen * minBlue;
+}
+
+fn get_min_num_of_cube(game: &Vec<Vec<(usize, &str)>>) -> (usize, usize, usize) {
+    // (red, gree, blue)
+    return game
+        .iter()
+        .fold((0, 0, 0), |fewest_num_of_cubes, current_packet| {
+            let curr_num_of_cubes = get_num_of_cube(current_packet);
+            return (
+                max(curr_num_of_cubes.0, fewest_num_of_cubes.0),
+                max(curr_num_of_cubes.1, fewest_num_of_cubes.1),
+                max(curr_num_of_cubes.2, fewest_num_of_cubes.2),
+            );
+        });
+}
+
+fn get_num_of_cube(current_packet: &Vec<(usize, &str)>) -> (usize, usize, usize) {
+    return current_packet
+        .iter()
+        .fold((0, 0, 0), |curr_num_of_cubes, curr_grab| match curr_grab.1 {
+            "red" => return (curr_grab.0, curr_num_of_cubes.1, curr_num_of_cubes.2),
+            "green" => return (curr_num_of_cubes.0, curr_grab.0, curr_num_of_cubes.2),
+            "blue" => return (curr_num_of_cubes.0, curr_num_of_cubes.1, curr_grab.0),
+            _ => panic!("couleur bizzare"),
+        });
+}
 
 fn line_is_valid(curr_line: &Vec<Vec<(usize, &str)>>) -> bool {
     return curr_line.iter().all(|packet| packet_is_valid(packet));
@@ -60,7 +95,7 @@ fn grab_is_valid(grab: (usize, &str)) -> bool {
         "red" => return grab.0 <= 12,
         "green" => return grab.0 <= 13,
         "blue" => return grab.0 <= 14,
-        _ => panic!("couleur bizzare")
+        _ => panic!("couleur bizzare"),
     }
 }
 
